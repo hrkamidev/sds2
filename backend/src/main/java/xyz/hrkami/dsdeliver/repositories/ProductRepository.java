0317cb15a010973c4e27bf513421f327f6081585
unknown <hrkamidev@gmail.com>
2021-07-31 09:26:06 -0300

    get /products

--- backend/src/main/java/xyz/hrkami/dsdeliver/repositories/ProductRepository.java
+++ backend/src/main/java/xyz/hrkami/dsdeliver/repositories/ProductRepository.java
@@ -1,9 +1,12 @@
 package xyz.hrkami.dsdeliver.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import xyz.hrkami.dsdeliver.entities.Product;
 
 public interface ProductRepository extends JpaRepository<Product, Long>{
 
+	List<Product> findAllByOrderByNameAsc();
 }
